68c5a8004ec7d9ce6850cd4d95d669a5a2e3dab8
Rolf Håvard Blindheim <rhblind@gmail.com>
2026-05-07 23:38:14 +0200

    feat(leader): Add private config file finder
    Add `scratch/find-file-in-user-config` command to quickly
    open files in the user config directory via SPC f p
    keybinding.

--- modules/editor/leader/file.el
+++ modules/editor/leader/file.el
@@ -2,9 +2,16 @@
 ;;
 ;; SPC f -- file-related leader bindings. Loaded by leader/config.el.
 
+(defun scratch/find-file-in-user-config ()
+  "Open a file picker scoped to `scratch-user-dir'."
+  (interactive)
+  (let ((default-directory (file-name-as-directory scratch-user-dir)))
+    (call-interactively #'find-file)))
+
 (map! :leader
   (:prefix-map ("f" . "file")
-   :desc "find file"   "f" #'find-file
-   :desc "save buffer" "s" #'save-buffer
-   :desc "save all"    "S" #'save-some-buffers
-   :desc "recent"      "r" #'recentf))
+   :desc "find file"          "f" #'find-file
+   :desc "save buffer"        "s" #'save-buffer
+   :desc "save all"           "S" #'save-some-buffers
+   :desc "recent"             "r" #'recentf
+   :desc "private config"     "p" #'scratch/find-file-in-user-config))
